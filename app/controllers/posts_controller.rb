6c6195b2bc269f361fe1fedd8498f1079321fc27
cstanda <standa.inc@gmail.com>
2015-02-01 20:03:31 +0000

    Controllers updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,23 +5,34 @@ class PostsController < ApplicationController
   end
   
   def show
-  
+    @post = Post.find(params[:id])
   end
   
   def new
-  
+    @post = Post.new
   end
   
   def create
-  
+    @post= Post.new(params[:post].permit(:title,:content))
+    #@post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path, :notice => "Your article was sucessfully posted!"
+    else
+      render 'new'
+    end
   end
   
   def edit
-  
+    @post = Post.find(params[:id])
   end
   
   def update
-  
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      redirect_to posts_path, :notice => "Your article has been updated!"
+    else
+      render "edit"
+    end
   end
   
   def destroy
