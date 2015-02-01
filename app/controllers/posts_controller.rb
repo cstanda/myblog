2233843c3233828467b19c55ee51153d4ccb5029
cstanda <standa.inc@gmail.com>
2015-02-01 20:23:56 +0000

    Destroy mehtod completed. Edit not working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,9 @@ class PostsController < ApplicationController
   end
   
   def destroy
-  
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :notice => "The article has been deleted!"
   end
   
 end
